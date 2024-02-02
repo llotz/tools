c71e856e8036756600ed61c99f358a7278c65247
Lukas Lotz <11498557+llotz@users.noreply.github.com>
2024-02-02 09:02:21 +0100

    Adding github link

--- index.php
+++ index.php
@@ -7,6 +7,6 @@
 
 This is a collection of _small_ tools I found myself rewriting in c# every time after switching employers.<br>
 Don't expect them to be killer but they made my life easier so maybe they can help you, too. <br>
-No input is logged here. I promise.<br>
+No input is logged here. I promise. And <a href="https://github.com/llotz/tools/">you can make sure</a> (github link)<br>
 <br>
 <div><a href="string-mapper">string mapper</a> - map values into strings.</div>
